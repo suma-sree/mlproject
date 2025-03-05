915426ac9da5f074527abfe9d900b61d86d77967
sai suma sree vankayalapati <vankayalapatisaisumasree2@gmail.com>
2025-03-05 17:07:31 +0530

    'setup'

---
+++ setup.py
@@ -0,0 +1,28 @@
+# building our application as a package
+
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    """
+    this function will return the list of requirements
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+
+
+setup(
+    name = 'mlproject',
+    version = '0.0.1',
+    author = 'suma sree',
+    author_email='vankayalapatisaisumasree2@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
